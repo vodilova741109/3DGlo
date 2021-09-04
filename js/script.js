577b69aa894b3ed6fbe694551ba6c7a5bd9c2fcd
Tatiana <vodilova@mail.ru>
2021-09-04 18:44:30 +0300

    Lesson26

--- js/script.js
+++ js/script.js
@@ -362,6 +362,74 @@ document.addEventListener("DOMContentLoaded", function(){
         }
       });
     }
-  calc(100); 
+    calc(100); 
+  
+  //send-ajax-form
+  const sendForm = () => {
+   const errorMessage = 'Что то пошло не так...',
+         loadMessage = 'Загрузка...',
+         successMesage = 'Спасибо мы скоро с вами свяжемся!';
+    const form = document.getElementById('form1'), 
+          form1 = document.querySelectorAll('form') ,  
+          statusMessage = document.createElement('div');       
+          statusMessage.style.cssText ='font-size: 2rem;';
+                
+    form.addEventListener('submit', (event) =>{
+      event.preventDefault();     
+      form.appendChild(statusMessage);
+      statusMessage.textContent = loadMessage;  
+      const formData = new FormData(form);
+      let body = {};
+      for (let val of formData.entries()){
+        body[val[0]] = val[1];
+      }
+      postData(
+        body, 
+        () =>{
+          statusMessage.textContent = successMesage;
+        }, 
+        (error) => {
+          statusMessage.textContent = errorMessage;
+          console.error(error);
+        }
+      );
+      
+    });
+    const postData = (body, outputData, errorData) =>{
+      const request = new XMLHttpRequest();
+      request.addEventListener('readystatechange', () => {
+        
+        if(request.readyState !== 4) {
+          return;
+        }
+        if (request.status === 200){
+          outputData();
+         
+        } else {
+          errorData(request.status);
+         
+        }
+      });
+
+      request.open('POST', './server.php');      
+      // request.setRequestHeader('Content-Type', 'multipart/form-data');
+      request.setRequestHeader('Content-Type', 'application/json');
     
+      
+      // request.send(formData); если через formData
+           request.send(JSON.stringify(body));
+
+           const inputForm = document.querySelectorAll('#form1 input');
+         
+           inputForm.forEach((item)  => {
+            if(body !== ''){
+              item.value = '';
+            } ;
+           })
+          
+    }
+   
+  }    
+  sendForm();
+
 });
