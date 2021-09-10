e9fce87ccd83b2991f95db831dcc3233d2408aa7
Tatyana.Vodilova <tatyana.vodilova@fabricmedia.ru>
2021-09-10 20:21:34 +0300

    Lesson 23+30

--- src/modules/regular.js
+++ src/modules/regular.js
@@ -15,42 +15,67 @@ let text = item.value;
 item.value = text.replace(/\D/g, '');       
 });
 });
-// имя
-name.forEach((item) => {
-  item.addEventListener('input', () => {          
-    let text = item.value;       
-    item.value = text.match(/[А-ЯЁа-яё\s-]+/ig);                
-  });
-  item.addEventListener('blur', () => {
-    let text = item.value;  
-    
-  // получаем массив из инпута имя 
-    let оbj = item.value.split(/\s|-/); 
-    // console.log(оbj);
-    // создаем новый пустой массив
-    let newObj =[];
-  // пробигаемся по каждому элементу массива 
-    оbj.forEach((item) =>{
-      // и если элемент не пустой
-      if (item !== ''){ 
-        // приводим все буквы к нижнему регистру  
-        let newText = item.toLowerCase();   
-        //  затем первую букву приводим к верхнему регистру первую букву и возвращаем все остальные в нижнем регистре
-        const nameNew = newText[0].toUpperCase() + newText.slice(1);    
-        // пушим все в новый объект         
-          newObj.push(nameNew);
-          console.log(newObj);
+  // имя
+  name.forEach((item) => {
+    item.addEventListener('input', () => {          
+      let text = item.value;       
+      item.value = text.match(/[А-ЯЁа-яё\s-]+/ig);                
+    });
+    item.addEventListener('blur', () => {
+      let text = item.value;  
+          // если дефис в начале или конце он их удаляет
+       if( (/^[\s-,]/.test(text)) || /[\s-,]$/.test(text)){              
+        item.value = text.replace(/^[\s-,]/g, '').trim();
+        let textNew = item.value; 
+        item.value = textNew.replace(/[\s-,]$/g, '').trim();    
       }   
-    });  
-    item.value = newObj.join('-');
-     if( (/^[\s-,]/.test(text)) || /[\s-,]$/.test(text)){              
-      item.value = text.replace(/^[\s-,]/g, '').trim();
-      let textNew = item.value; 
-      item.value = textNew.replace(/[\s-,]$/g, '').trim();    
-    }   
+     
+      // получаем массив из инпута имя 
+      let оbj = item.value.split(' '); 
+     
+      // создаем новый пустой массив
+      let newObj =[];
+    // пробигаемся по каждому элементу массива 
+      оbj.forEach((item) =>{
+        // и если элемент не пустой
+        if (item !== '' ){ 
+          // приводим все буквы к нижнему регистру  
+          let newText = item.toLowerCase();   
+          //  затем первую букву приводим к верхнему регистру первую букву и возвращаем все остальные в нижнем регистре
+          const nameNew = newText[0].toUpperCase() + newText.slice(1);    
+          // пушим все в новый объект         
+            newObj.push(nameNew);
+            console.log(newObj);
+        }   
+      });  
+      item.value = newObj.join(' ');
+    //  проверка на дефис
+      if(/-/.test(оbj)){
+            // получаем массив из инпута имя 
+        let оbjВash  = item.value.split('-'); 
+        console.log(оbjВash);
+        // создаем новый пустой массив
+        let newObjBash =[];
+      // пробигаемся по каждому элементу массива 
+        оbjВash.forEach((item) =>{
+          // и если элемент не пустой
+          if (item !== ''){ 
+            // приводим все буквы к нижнему регистру  
+            let newText = item.toLowerCase(); 
+            
+            //  затем первую букву приводим к верхнему регистру первую букву и возвращаем все остальные в нижнем регистре
+            const nameNew = newText[0].toUpperCase() + newText.slice(1);    
+            console.log(nameNew);
+            // пушим все в новый объект         
+              newObjBash.push(nameNew);
+              console.log(newObjBash);
+          }   
+        });  
+        item.value = newObjBash.join('-');
+      }
    
+    });
   });
-});
 
 
 // сообщение
